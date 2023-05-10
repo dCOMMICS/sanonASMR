876a1d73a294adc09a874c8d2f32be6173500628
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-10 16:47:06 +0300

    More Code

--- game.js
+++ game.js
@@ -3,4 +3,6 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 
-const pacmanFrames = document.getElementById("animations")
+const pacmanFrames = document.getElementById("animations");
+
+const ghostFrames = document.getElementById("ghost")
