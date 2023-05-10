b6871dbb20cc4b74f956d923b591cd408768e1c4
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-10 16:51:33 +0300

    more info

--- game.js
+++ game.js
@@ -5,4 +5,12 @@ const canvasContext = canvas.getContext("2d");
 
 const pacmanFrames = document.getElementById("animations");
 
-const ghostFrames = document.getElementById("ghost")
+const ghostFrames = document.getElementById("ghost");
+
+let createRec = (x,y,width,height,color) =>{
+canvasContext.fillStyle = color;
+canvasContext.fillRect(x,y, width,height);
+};
+
+// mapp area coded
+let map
