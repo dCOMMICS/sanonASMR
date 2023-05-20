c63cee9f60d86331126459b1bd030e03c93ba5be
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-20 13:27:43 +0300

    strings

--- Travel App/home.dart
+++ Travel App/home.dart
@@ -53,3 +53,22 @@ void main() {
   var numbers = List.generate(10, (i) => i);
   print(getEvenNumbers(numbers));
 }
+
+import 'dart:math' as math;
+
+void main() {
+  print('a single quoted string');
+  print("a double quoted string");
+
+  // Strings can be combined by placing them adjacent to each other.
+  print('cat' 'dog');
+
+  // Triple quotes define a multi-line string.
+  print('''triple quoted strings
+are for multiple lines''');
+
+  // Dart supports string interpolation.
+  final pi = math.pi;
+  print('pi is $pi');
+  print('tau is ${2 * pi}');
+}
