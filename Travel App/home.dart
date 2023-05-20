afc9a7684fce6cf0e3d3931e5c28a5d7ac80879f
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-20 13:27:11 +0300

    loops

--- Travel App/home.dart
+++ Travel App/home.dart
@@ -24,3 +24,32 @@ void main() {
   print('4 times four is ${timesFour(4)}');
   print('2 x 2 x 2 is ${runTwice(2, timesTwo)}');
 }
+
+
+bool isEven(int x) {
+  // An if-else statement.
+  if (x % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+List<int> getEvenNumbers(Iterable<int> numbers) {
+  var evenNumbers = <int>[];
+
+  // A for-in loop.
+  for (var i in numbers) {
+    // A single line if statement.
+    if (isEven(i)) {
+      evenNumbers.add(i);
+    }
+  }
+
+  return evenNumbers;
+}
+
+void main() {
+  var numbers = List.generate(10, (i) => i);
+  print(getEvenNumbers(numbers));
+}
