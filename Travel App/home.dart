f90d0189fd79d70a757408fafb01aa9d6ae7f2c3
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-20 13:28:13 +0300

    more

--- Travel App/home.dart
+++ Travel App/home.dart
@@ -72,3 +72,27 @@ are for multiple lines''');
   print('pi is $pi');
   print('tau is ${2 * pi}');
 }
+
+
+// A list literal.
+const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+// A map literal.
+const nobleGases = {
+  'He': 'Helium',
+  'Ne': 'Neon',
+  'Ar': 'Argon',
+};
+
+// A set literal.
+const frogs = {
+  'Tree',
+  'Poison dart',
+  'Glass',
+};
+
+void main() {
+  print(lostNumbers.last);
+  print(nobleGases['Ne']);
+  print(frogs.difference({'Poison dart'}));
+}
